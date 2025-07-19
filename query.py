fd3ba54992571ea96581b6e22d3df288abbb577b
Ali Mohammadjani <rura565700@gmail.com>
2025-07-19 07:30:51 +0330

    main

--- query.py
+++ query.py
@@ -1,3 +1,12 @@
 import sqlite3
-import pandas as pandas
+import pandas as pd
 
+conn=sqlite3.connect("movie.db")
+
+query="""
+SELECT *
+FROM movie;
+"""
+df=pd.read_sql_query(query,conn)
+conn.close()
+print(df)
--- sql.py
+++ sql.py
@@ -4,10 +4,10 @@ conn=sqlite3.connect("movie.db")
 cursor=conn.cursor()
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS movie(
-    Id INTEGER
-    Title TEXT
-    Director TEXT
-    Year INTEGER
+    Id INTEGER,
+    Title TEXT,
+    Director TEXT,
+    Year INTEGER,
     Length_minutes INTEGER
 );
 """)
