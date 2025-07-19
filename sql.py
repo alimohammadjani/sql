adb453f332c44f12fd61a54fdc9178f11240d11a
Ali Mohammadjani <rura565700@gmail.com>
2025-07-19 05:25:39 +0330

    main

--- sql.py
+++ sql.py
@@ -11,3 +11,11 @@ CREATE TABLE IF NOT EXISTS movie(
     Length_minutes INTEGER
 );
 """)
+movie_data=[
+    (1,"toy story","john lasseter",1995,81),
+    (2, 'A Bug''s Life', 'John Lasseter', 1998, 95),
+    (3, 'Toy Story 2', 'John Lasseter', 1999, 93),
+    (4, 'Monsters, Inc.', 'Pete Docter', 2001, 92),
+    (5, 'Finding Nemo', 'Andrew Stanton', 2003, 107)
+]
+cursor.executemany("INSERT INTO movie VALUES (?,?,?,?,?);", movie_data)
